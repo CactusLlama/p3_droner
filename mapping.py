1aeba296ed4106a632cf7c9e1b220be76c48e9c8
MarcTBP <92086146+MarcTBP@users.noreply.github.com>
2022-12-08 10:20:52 +0100

    mapping added as a new file

---
+++ mapping.py
@@ -0,0 +1,161 @@
+import math
+from djitellopy import tello
+import KeyPressModule as kp
+import numpy as np
+import cv2
+from time import sleep
+
+################ Parameters ##################
+forwardS = 200/10  # Forward Speed in cm/s (at the speed 15cm/s = 11.7cm/10s) TEST THIS!
+rotationS = 360/10  # Rotation speed in degrees/second (10s to rotate 360 degrees) (at 50d/s)
+interval = 0.25
+
+distInterval = forwardS*interval  # 20*0.25= 5
+rotationInterval = rotationS*interval  # 36 * 0.25 = 9 (this is only used for keyboard input)
+##############################################
+x = 500
+y = 500
+a = 0
+yaw = 0
+dist = [int, int]
+
+
+print("Input the point where the drone has to go (in cm):\n")
+dist[0] = int(input("Input x: "))
+dist[1] = int(input("Input y: "))
+
+print(dist)
+
+total_distance  = dist[0]*dist[0]+dist[1]*dist[1]
+total_distance  = math.sqrt(total_distance)
+print("total distance from start to distination:", total_distance)
+
+plot_point = [dist[0]+500, -1*dist[1]+500]
+
+kp.init()
+
+# me = tello.Tello()
+# me.connect()
+
+points = [(0, 0), (0, 0)]
+
+
+def getKeyboardInput():
+    lr, fb, ud, yv, tl = 0, 0, 0, 0, 0
+    speed = 15
+    aspeed = 50
+
+    global x, y, yaw, a
+    d = 0
+
+    if kp.getKey("LEFT"):
+        lr = -speed
+        d = distInterval
+        a = -180
+
+    elif kp.getKey("RIGHT"):
+        lr = speed
+        d = -distInterval
+        a = 180
+
+    if kp.getKey("UP"):
+        fb = speed
+        d = distInterval
+        a = 270
+
+    elif kp.getKey("DOWN"):
+        fb = -speed
+        d = -distInterval
+        a = -90
+    if kp.getKey("w"):
+        ud = speed
+
+    elif kp.getKey("s"):
+        ud = -speed
+
+    if kp.getKey("a"):
+        yv = aspeed
+        yaw -= rotationInterval
+    elif kp.getKey("d"):
+        yv = -aspeed
+        yaw += rotationInterval
+
+    if kp.getKey("q"):
+        tl = 1
+    elif kp.getKey("e"):
+        tl = 2
+
+    sleep(interval)
+    a += yaw
+    x += int(d * math.cos(math.radians(a)))
+    y += int(d * math.sin(math.radians(a)))
+
+    return [lr, fb, ud, yv, tl, x, y]
+
+
+
+def drawPoints(img, points):
+    for point in points:
+        cv2.circle(img, point, 5, (0, 0, 255), cv2.FILLED)
+
+    cv2.circle(img, points[-1], 8, (255, 0, 0), cv2.FILLED)
+    cv2.putText(img, f'({(points[-1][0]-500)/100}, {-1*((points[-1][1]-500)/100)})m',
+                (points[-1][0]+10, points[-1][1]+30), cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 255), 1)
+
+
+if __name__ == "__main__":
+    vals = [0, 0, 0, 0, 0, x, y]
+    # me.takeoff()
+    # sleep(4)
+    keyboard_command = getKeyboardInput()
+    while True:
+        # print(me.get_battery())
+        print(vals)
+
+        location = [x, y]
+        destination = [dist[0], dist[1]]
+        destMinusLoca = [dist[0]-(location[0]-500), dist[1]-(location[1]-500)]
+
+        # Calculate angle differences and adjust to 0
+        ang_adjust_radians = math.atan2(destMinusLoca[0], destMinusLoca[1])
+        ang_adjust_degrees = (180/math.pi)*ang_adjust_radians
+
+        ang_adjust_degrees = int(ang_adjust_degrees)
+        print(ang_adjust_degrees)
+        print(-ang_adjust_degrees)
+
+        if yaw != ang_adjust_degrees and ang_adjust_degrees > 0:
+            # me.rotate_clockwise(ang_adjust_degrees)
+            # sleep(6)
+            yaw = ang_adjust_degrees
+        elif yaw != ang_adjust_degrees and ang_adjust_degrees < 0:
+            # me.rotate_counter_clockwise(-ang_adjust_degrees)
+            # sleep(6)
+            yaw = ang_adjust_degrees
+
+        # Fly forward at the speed of 15cm/s until destination is reached
+        distance = 0
+        if vals[5] == plot_point[0] and vals[6] == plot_point[1] or vals[5] == plot_point[0]+1 and vals[6] == plot_point[1]+1 \
+                or vals[5] == plot_point[0]-1 and vals[6] == plot_point[1]-1:
+            print("Landing")
+            # me.land()
+        else:
+            vals[1] = 10
+            distance = distInterval
+            a = 270
+
+        a += yaw
+        vals[5] += int(distance * math.cos(math.radians(a)))
+        vals[6] += int(distance * math.sin(math.radians(a)))
+
+        # me.send_rc_control(vals[0], vals[1], vals[2], vals[3])
+        print(vals)
+        sleep(interval)
+
+        # Display map, mapping drone on screen
+        img = np.zeros((1000, 1000, 3), np.uint8)
+        points.append((vals[5], vals[6]))
+        cv2.circle(img, plot_point, 5, (0, 255, 0), cv2.FILLED)
+        drawPoints(img, points)
+        cv2.imshow("Output", img)
+        cv2.waitKey(1)
